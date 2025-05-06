70754fd9c883f86d074ff181d606d9dc92b39450
amit-pal-maersk <141243958+amit-pal-maersk@users.noreply.github.com>
2025-05-06 12:16:41 +0530

    Update test.sql

--- src/main/resources/db/changelog/test.sql
+++ src/main/resources/db/changelog/test.sql
@@ -2,9 +2,9 @@
 --changeset Amit:insert_2
 --tag v2.0
 insert into person (name, address1, address2, city) values
-    ('John Doe', '123 Elm St', 'Apt 4B', 'Springfield'),
-    ('Jane Smith', '456 Oak St', '', 'Shelbyville'),
-    ('Bob Johnson', '789 Pine St', 'Suite 3C', 'Ogdenville');
+    ('amit', '123 Elm St', 'Apt 4B', 'Springfield'),
+    ('pal', '456 Oak St', '', 'Shelbyville'),
+    ('sudeep', '789 Pine St', 'Suite 3C', 'Ogdenville');
 
 insert into company (name, address1, address2, city) values
     ('Acme Corp', '500 Industrial Rd', 'Suite 100', 'Springfield'),
